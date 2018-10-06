b713b5a98654ce59def8afe3dd4d40e231139087
J Young Kim <thejykoder@gmail.com>
2018-10-06 04:48:03 -0400

    Firestore

--- Synoptica-Web App/index.js
+++ Synoptica-Web App/index.js
@@ -1,19 +1,34 @@
 var express = require('express');
+
 var app = express();
+var server =  require('http').createServer(app);
 var path = require('path');
 var hbs = require('hbs');
-
+var io = require('socket.io').listen(server);
 
 // Imports the Google Cloud client library
 const language = require('@google-cloud/language');
+const admin = require('firebase-admin');
+var defaultapp = admin.initializeApp();
+const db = admin.firestore();
 
 // Instantiates a client
 const client = new language.LanguageServiceClient();
 
-
 app.set('port', process.env.PORT || 8000 );
 app.set('view engine', 'hbs');
 
+var database = [];
+
+db.collection('patients').get().then(snapshot => {
+  snapshot.docs.forEach(doc => {
+      add(doc);
+  });
+});
+
+function add(a) {
+  database[a.id] = a.data();
+}
 
 app.get('/', function(req, res){
     res.render('index');
@@ -24,11 +39,10 @@ app.get('/dashboard', function(req, res){
 });
 
 app.get('/patient', function(req, res){
-	
 
+  console.log(database);
   /*GET TEXT FROM CLOUD*/
   const text = 'I love this world';
-
   const document = {
     content: text,
     type: 'PLAIN_TEXT',
@@ -39,7 +53,6 @@ app.get('/patient', function(req, res){
   .analyzeSentiment({document: document})
   .then(results => {
     const sentiment = results[0].documentSentiment;
-    
     patientInfo = { 
         pfp: "https://images-na.ssl-images-amazon.com/images/I/71TTbee5oKL._SX466_.jpg",
         name: "Joey",
@@ -52,9 +65,16 @@ app.get('/patient', function(req, res){
   })
   .catch(err => {
     console.error('ERROR:', err);
+    res.render('error');
   });	  
 });
 
-var listener = app.listen(app.get('port'), function() {
-  console.log( 'Express server started on port: '+listener.address().port );
+server.listen(8000, function(){
+
+});
+
+io.on('connection', function(socket) {
+   socket.on('henlo', function () {
+      console.log('A user disconnected');
+   });
 });
