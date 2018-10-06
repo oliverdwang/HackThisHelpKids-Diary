0e0346b6530a7fbda8980239e3d14bf0ee957f01
J Young Kim <thejykoder@gmail.com>
2018-10-06 00:02:59 -0400

    Implemented Google Cloud Language

--- Synoptica-Web App/index.js
+++ Synoptica-Web App/index.js
@@ -3,6 +3,14 @@ var app = express();
 var path = require('path');
 var hbs = require('hbs');
 
+
+// Imports the Google Cloud client library
+const language = require('@google-cloud/language');
+
+// Instantiates a client
+const client = new language.LanguageServiceClient();
+
+
 app.set('port', process.env.PORT || 8000 );
 app.set('view engine', 'hbs');
 
@@ -13,6 +21,38 @@ app.get('/', function(req, res){
 
 app.get('/dashboard', function(req, res){
     res.render('dashboard');
+});
+
+app.get('/patient', function(req, res){
+	
+
+  /*GET TEXT FROM CLOUD*/
+  const text = 'I love this world';
+
+  const document = {
+    content: text,
+    type: 'PLAIN_TEXT',
+  };
+
+  // Detects the sentiment of the text
+  client
+  .analyzeSentiment({document: document})
+  .then(results => {
+    const sentiment = results[0].documentSentiment;
+    
+    patientInfo = { 
+        pfp: "https://images-na.ssl-images-amazon.com/images/I/71TTbee5oKL._SX466_.jpg",
+        name: "Joey",
+        age: 14,
+        symptoms: "Cancer",
+        score: sentiment.score
+    };
+
+    res.render('patient', patientInfo);
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });	  
 });
 
 var listener = app.listen(app.get('port'), function() {
